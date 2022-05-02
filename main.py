a39154194ad42fcfd455e1e8b0c44031cb396d13
Nguyên Anh <nngguyen.anh@gmail.com>
2022-05-02 21:38:40 +0700

    clean code

--- main.py
+++ main.py
@@ -3,26 +3,26 @@ from Machine.index import Machine
 
 dataCenter = DataCenter("connect4.data")
 
-# dataCenter.separateIntoRatioAccording()
-
+# run for many ratio
 ratioList = [[90,10],[80,20],[60,40],[40,60]]
-# for ratio in ratioList:
-#     dataCenter.separateIntoRatioAccording(ratio)
-#     machine = Machine(ratio)
-#     machine.train()
-#     machine.test()
-#     machine.plotConfusionMatrix()
-#     #machine.drawDecisionTree()
-#     print('\n' * 2)
-
-for i in range(2,8):
-    dataCenter.separateIntoRatioAccording([80,20])
-    machine = Machine([80,20],i)
+for ratio in ratioList:
+    dataCenter.separateIntoRatioAccording(ratio)
+    machine = Machine(ratio)
     machine.train()
     machine.test()
     machine.plotConfusionMatrix()
-    machine.drawDecisionTree()
-    print('\n\n')
+    #machine.drawDecisionTree()
+    print('\n' * 2)
+
+# run for many max_depth 
+# for i in range(2,8):
+#     dataCenter.separateIntoRatioAccording([80,20])
+#     machine = Machine([80,20],i)
+#     machine.train()
+#     machine.test()
+#     machine.plotConfusionMatrix()
+#     machine.drawDecisionTree()
+#     print('\n\n')
 
 
 
